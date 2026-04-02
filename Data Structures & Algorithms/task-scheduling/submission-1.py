1fdd2617efe7c9195ff5b5fe57091167e758aaa0
Prajjwal Mehta <76418595+PRAJJWALmehta@users.noreply.github.com>
2026-04-02 20:56:36 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = [0]*26
+        
+        for task in tasks:
+            count[ord(task)-ord('A')] += 1
+        count.sort()
+
+        maxFreq = count[25]
+        idle = (maxFreq-1)*n
+
+        for i in range(24, -1, -1):
+            idle -= min(maxFreq-1, count[i])
+
+        return max(0, idle) + len(tasks)
