4c6b4865c3d1aafc11432f8bf102b3289be5e9da
Prajjwal Mehta <76418595+PRAJJWALmehta@users.noreply.github.com>
2026-04-07 22:24:52 +0530

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def _isPalindrome(self, s: str, i: int, j: int) -> bool:
+        l, r = i, j
+
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        
+        return True
+
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        part = []
+
+        def dfs(i: int):
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            
+            for j in range(i, len(s)):
+                if self._isPalindrome(s, i, j):
+                    part.append(s[i:j+1])
+                    dfs(j+1)
+                    part.pop()
+
+        
+        dfs(0)
+        return res
+        
