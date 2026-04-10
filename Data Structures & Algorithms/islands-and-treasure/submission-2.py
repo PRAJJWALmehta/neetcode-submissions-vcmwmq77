4ae974b68805d80637cb7e610ae2acab7a31191e
Prajjwal Mehta <76418595+PRAJJWALmehta@users.noreply.github.com>
2026-04-11 04:36:51 +0530

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        land_cell = 2**31-1
+        q = deque()
+
+        # place all the treasure cells in the queue
+        for r in range(ROWS):
+            for c in range(COLS):
+                if not grid[r][c]:
+                    q.append((r, c))
+        
+
+        # start computing the distance in all directions of the treasure cells
+        while q:
+            print(q)
+            length = len(q)
+            for i in range(length):
+                row, col = q.popleft()
+                
+                for dr, dc in directions:
+                    r = row + dr
+                    c = col + dc
+                    if r >= 0 and r < ROWS and c >= 0 and c < COLS and grid[r][c] == land_cell:
+                        grid[r][c] = grid[row][col] + 1
+                        q.append((r, c))
+            
