d146117f10da2452fa9295b8b5d7e4581e98f766
Prajjwal Mehta <76418595+PRAJJWALmehta@users.noreply.github.com>
2026-05-26 01:48:29 +0530

    Add: reverse-integer - submission-2

---
+++ Data Structures & Algorithms/reverse-integer/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if not x:
+            return 0
+        neg = False
+        if x < 0:
+            x = abs(x)
+            neg = True
+        n = int(math.log(x, 10))
+        res = 0
+
+        
+        for i in range(n+1):
+            d = x%10
+            res += d*pow(10, n-i)
+            x = x//10
+        if res >= pow(2, 31):
+            return 0
+            
+        return res*pow(-1, int(neg))
