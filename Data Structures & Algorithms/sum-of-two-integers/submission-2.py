9b3996bb080c3ca145470c906326c147f641e5de
Prajjwal Mehta <76418595+PRAJJWALmehta@users.noreply.github.com>
2026-05-26 01:00:17 +0530

    Add: sum-of-two-integers - submission-2

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        res = 0
+        c = 0
+        mask = 0xFFFFFFFF
+
+        for i in range(32):
+            ab = (a>>i)&1
+            bb = (b>>i)&1
+
+            s = ab^bb^c
+            c = ab&bb | bb&c | c&ab
+
+            res |= (s<<i)
+        
+        return ~(res^mask) if (res & (1<<31)) else res
